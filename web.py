fe4423b0463b38ee65137f13954e47dd79382e74
souparno1407 <32390635+souparno1407@users.noreply.github.com>
2019-03-19 20:34:54 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-
+# Creating cascade classifiers for face and eye
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+# Capturing video. 0 is passed to set it to the default webcam
 video_capture = cv2.VideoCapture(0)
 
 while True:
@@ -14,9 +14,10 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Multi-scale is used to analyze the image with various scales so that it can succesfully detect small faces as well
     faces = faceCascade.detectMultiScale(
         gray,
-        scaleFactor=1.25,
+        scaleFactor=1.25, # You may play around with this scaling factor to obtain better results
         minNeighbors=5,
         minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE
@@ -28,6 +29,7 @@ while True:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         
+    # Do the same thing for eyes
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex,ey,ew,eh) in eyes:
         cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
@@ -35,9 +37,10 @@ while True:
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
+    #Press q to terminate the program or else go on detecting
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything is done, release the capture
+# Release the capture after pressing the quit button and destroy the windows
 video_capture.release()
 cv2.destroyAllWindows()
